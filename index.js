26dadc09416c6a02552bfba2cf62c61d5d2c5648
AngelRetana <angel.gabriel.retana.rivas@ibm.com>
2019-05-13 11:43:23 -0500

    add config to server

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const colors = require('colors');
-
+//start the server with express and seve in to a variable 
 const server = express();
 
 server.get('/', function(req, res) {
     res.send('<h1>Hola mundo desde express y node</h1>');
     res.end();
 });
+//defin in witch port the server will start 
 server.listen(3000, function() {
     console.log('server on port 3000'.green);
 });
